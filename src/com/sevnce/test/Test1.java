fd34e236905c20995048fd744e0cccc496c17ff1
tangzixiang <tzixiang@foxmail.com>
2017-08-22 10:21:18 +0800

    创建项目

---
+++ src/com/sevnce/test/Test1.java
@@ -0,0 +1,15 @@
+package com.sevnce.test;
+/** 
+ * 
+ * @author tzx 
+ * @version 
+ * 创建时间：2017年8月22日 上午10:14:50  
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		String str = "This is the test class.";
+		System.out.println(str);
+	}
+
+}
